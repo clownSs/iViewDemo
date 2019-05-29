9570f195a1ee0e5f981c05523a1b0e2358fe0024
xhc_glb <xhc_glo@123.com>
2019-05-29 12:29:52 +0800

    iViewDemo项目创建了

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -8,16 +7,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      component: () => import('./views/Login/Login.vue')
+    },
+    {
+      path: '/main',
+      name: 'main',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/Main/Main.vue')
     }
   ]
 })
---
+++ src/views/Backstage/Backstage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Backstage
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Backstage"
+  }
+</script>
+
+<style scoped>
+
+</style>
